adbcc0a9e45fb551911ff53ecf35d49c01da7d88
Smarthakeem <110948002+Smarthakeem@users.noreply.github.com>
2022-12-19 16:43:12 +0100

    function that returns string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,33 +1,16 @@
 #include "main.h"
-
-
-
 /**
- *
- *  * _strlen - function that returns the length of a string.
- *
- *   * @s : s is a character
- *
- *    * Return: value is i
- *
- *     */
-
+ * _strlen - return the length of a string
+ * Owned By Bwave
+ * @s: char to check
+ * Description: this will return the length of a string
+ * Return: 0 is success
+ */
 int _strlen(char *s)
-
 {
+	int a = 0;
 
-		int i;
-
-
-
-			for (i = 0; s[i] != '\0'; i++)
-
-					{
-
-
-
-							}
-
-			return (i);
-
+	for (; *s++;)
+		a++;
+	return (a);
 }

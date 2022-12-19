5d20e6d66cef224ad08762db87c77fc81a1c4eba
Smarthakeem <110948002+Smarthakeem@users.noreply.github.com>
2022-12-19 17:08:39 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,79 +1,40 @@
 #include "main.h"
 
-
-
 /**
+ * _atoi - Convert a string to an integer.
+ * @s: The pointer to convert
  *
- *  * _atoi - Convert a string to an integer.
- *
- *   * @s: The pointer to convert
- *
- *    *
- *
- *     * Return: A integer
- *
- *      */
-
+ * Return: A integer
+ */
 int _atoi(char *s)
-
 {
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-		int c = 0;
+	while (s[c])
+	{
+		if (s[c] == 45)
+		{
+			min *= -1;
+		}
 
-			unsigned int ni = 0;
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+		}
 
-				int min = 1;
+		if (isi == 1)
+		{
+			break;
+		}
 
-					int isi = 0;
+		c++;
+	}
 
-
-
-						while (s[c])
-
-								{
-
-											if (s[c] == 45)
-
-														{
-
-																		min *= -1;
-
-																				}
-
-
-
-													while (s[c] >= 48 && s[c] <= 57)
-
-																{
-
-																				isi = 1;
-
-																							ni = (ni * 10) + (s[c] - '0');
-
-																										c++;
-
-																												}
-
-
-
-															if (isi == 1)
-
-																		{
-
-																						break;
-
-																								}
-
-
-
-																	c++;
-
-																		}
-
-
-
-							ni *= min;
-
-								return (ni);
-
+	ni *= min;
+	return (ni);
 }

1bd2adaf4a378d5173332eb43e5dcd24fe3db236
Smarthakeem <110948002+Smarthakeem@users.noreply.github.com>
2022-12-19 16:31:06 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,69 +1,17 @@
 #include "main.h"
-
-
-
 /**
+ * puts2 - prints every other character of a string
  *
- * * _strlen - returns the length of a string.
+ * @str: char to check
  *
- * * @s: a pointer to a char
- *
- * * Return: length of a string
- *
- * **/
-
-
-
-int _strlen(char *s)
-
-{
-
-		int l, i = 0;
-
-
-
-		while (s[i] != '\0')
-
-			{
-
-				i++;
-
-			}
-
-		l = i;
-
-		return (l);
-
-}
-
-
-
-/**
- *
- * * puts2 - prints one char out of 2 of a string
- *
- * * @str: a pointer to string
- *
- * * Return: nothing
- *
- * **/
-
-
-
+ * Return: 0 is success
+ */
 void puts2(char *str)
-
 {
+	int string;
 
-		int i;
-
-		int size = _strlen(str);
-
-		for (i = 0; i < size; i += 2)
-
-			{
- 				_putchar(str[i]);
-			}
-
-		_putchar('\n');
-
+	for (string = 0; str[string] != '\0'; string++)
+	if (string % 2 == 0)
+		_putchar(str[string]);
+	_putchar('\n');
 }

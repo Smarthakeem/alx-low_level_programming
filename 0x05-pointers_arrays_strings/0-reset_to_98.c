697eb162f2442f1cc0f64ed011b46d2aa2a7f64f
Smarthakeem <110948002+Smarthakeem@users.noreply.github.com>
2022-12-19 16:47:54 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,25 +1,12 @@
 #include "main.h"
-
-
-
 /**
- *
- * * reset_to_98 -  takes a pointer to an int
- *
- * * @n: take an int
- *
- * * Return: int
- *
- * **/
-
-
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to check
+*  Owned By Bwave/Bright
+*  Return: 0 is success
+*/
 
 void reset_to_98(int *n)
-
 {
-
-		*n = 98;
-
+	*n = 98;
 }
-
-

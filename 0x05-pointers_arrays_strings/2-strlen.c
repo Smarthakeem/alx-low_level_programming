acb354b030f0deb72529584d8bf7638e7ab0ea99
Smarthakeem <110948002+Smarthakeem@users.noreply.github.com>
2022-12-19 16:45:35 +0100

    Function that swaps value of integer

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _strlen - return the length of a string
- * Owned By Bwave
- * @s: char to check
- * Description: this will return the length of a string
- * Return: 0 is success
- */
-int _strlen(char *s)
+* swap_int - swaps the value of int a and int b
+* Owned by Bwave/Bright
+* @a: first int
+* @b: second int
+* Return 0
+*/
+void swap_int(int *a, int *b)
 {
-	int a = 0;
+		int s;
 
-	for (; *s++;)
-		a++;
-	return (a);
+		s = *a;
+		*a = *b;
+		*b = s;
 }

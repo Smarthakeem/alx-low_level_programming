5d17334d88e0b4bf515e67699bc939c768526839
Smarthakeem <110948002+Smarthakeem@users.noreply.github.com>
2022-12-19 16:50:27 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,25 +1,16 @@
 #include "main.h"
-
-
-
 /**
- *
- * * swap_int - swaps the values of two integers
- *
- * * @a: take an int
- *
- * * @b: take an int
- *
- * **/
-
+* swap_int - swaps the value of int a and int b
+* Owned by Bwave/Bright
+* @a: first int
+* @b: second int
+* Return 0
+*/
 void swap_int(int *a, int *b)
-
 {
+		int s;
 
-		int i = *a;
-
+		s = *a;
 		*a = *b;
-
-		*b = i;
-
+		*b = s;
 }
